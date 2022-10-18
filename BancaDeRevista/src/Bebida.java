845eee892b98670284c62b5f1424fb0261f74e8f
Lucemy Ferreira <lucemyjunior@hotmail.com>
2022-10-18 15:57:13 -0300

    Adding Content

---
+++ BancaDeRevista/src/Bebida.java
@@ -0,0 +1,17 @@
+public class Bebida extends Produto {
+    
+    private String tipo;
+
+    public Bebida(int codigo, String nome, String tipo){
+        super(codigo, nome);
+        this.tipo = tipo;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+}
